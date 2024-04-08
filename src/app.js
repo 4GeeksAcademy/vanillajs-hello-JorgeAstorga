0d0c3fc972618ff88d14309e212e3409f863cad1
8u1137pr00f <162195925+8u1137pr00f@users.noreply.github.com>
2024-04-08 13:53:48 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 /* eslint-disable */
+// no elimino esto por si afecta el estilo del fixed-bottom p-4
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //variables let para definir cada parte
+  let who = ["The dog", "My grandma", "The mailman", "My bird", "An Alien"];
+  let action = ["ate", "peed", "crushed", "broke", "vaporized"];
+  let what = ["my homework", "my phone", "the car", "my wife", "my beloved PC"];
+  let when = [
+    "before the class.",
+    "when I was sleeping.",
+    "while I was exercising.",
+    "during my lunch.",
+    "while I was praying.",
+    "for some secret reason only the government knows. Ssshhh..."
+  ];
+
+  //funciones random que seleccionan de forma aleatoria un elemento de cada array "let"
+  function generateExcuse() {
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+  }
+  //console.log(generateExcuse) no tendría que usarlo porque se usa document
+  document.getElementById("excusa").innerHTML = generateExcuse();
 };
